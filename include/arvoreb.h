df603dd85c61aa9a19cb24cab542d08bd89f41ca
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2024-08-02 17:51:52 -0300

    Update arvoreb.h

--- include/arvoreb.h
+++ include/arvoreb.h
@@ -1,7 +1,7 @@
 #ifndef ARVOREB_H
 #define ARVOREB_H
-
 #include "../include/item.h"
+
 #define M 10// Ordem da árvore
 
 //criando variável ponteiro do tipo página
@@ -14,12 +14,10 @@ typedef struct Pagina {
 } Pagina; 
 
 void inicializa(Apontadores* Arvore);
-
-//void arvoreB(Analise*, FILE*, TipoRegistro *, int , int, int);
-
 void pesquisar(Item* x, Apontadores Arvore, Analise*);
 void insere(Item x, Apontadores* Arvore, Analise*); 
 void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apontadores* ApRetorno, Analise*);
+void criaarvoreb(Analise *analise, FILE *arquivo, Item *registro, int situacao, long int quant, int metodo);
 void inserenaPagina(Apontadores Arvore, Item dados, Apontadores ApDir, Analise*);
 void imprimir(Item x); // impressão das informações da chave encontrada. 
 
