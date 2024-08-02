20e45f74dff9e57a74e78d1427ac9cbd5156b649
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2024-08-02 17:31:34 -0300

    Update arvoreb.c

--- src/arvoreb.c
+++ src/arvoreb.c
@@ -1,37 +1,86 @@
 #include "../include/arvoreb.h"
+#include "../include/item.h"
+
+#include <stdio.h>
+#include <stdbool.h>
 
 #define M 10// Ordem da árvore
 
-void inicializa(Apontadores Arvore){
-    Arvore = NULL; 
+void inicializa(Apontadores* Arvore){// cria apontador para a arvore
+    *Arvore = NULL; 
 }
 
-void inserenaPagina(Apontadores Arvore, Item dados, Apontadores ApDir){
+void criaarvoreb(Analise *analise, FILE *arquivo, Item *registro, int situacao, long int quant, int metodo){
+
+    Apontadores Arvore;
+    clock_t timepre;//tempo na pre processamento
+    clock_t timepesquisa;//tempo de pesquisa 
+
+     //dados de analise do processo 
+    Inicializa(&Arvore);
+    analise->comppesquisa = 0;//comparações na pesquisa
+    analise->comppre = 0; //comparações no pré processamento
+    analise->transpre = 0;///transferencia no pré processamento
+    analise->transpesquisa = 0;//transferencia na pesquisa
+
+    int i = 0;
+
+    timepre = clock();// marcar o tempo de inicio do processamento
+
+    //lendo dados do arquivo e inserindo na arvore
+    while((fread(&registro, sizeof(Item), 1, arquivo) == 1) && i<quant){     
+           if (metodo == 1){
+            printf("Chave %ld\n", registro->chave);
+        }
+        i++;
+        insere(*registro, &Arvore, analise);
+    } 
+
+    timepre = clock() - (double)timepre; // fim do processamento 
+    //numero de comparações ao inserir dados
+    analise->comppre = ((double)timepre) / CLOCKS_PER_SEC;
+
+    timepesquisa = clock();//guarda o tempo inicial da pesquisa 
+    pesquisar(registro, Arvore, analise);
+
+    timepesquisa = clock() - (double)timepesquisa;//marca fim da pesquisa e realiza calcula a diferença de tempo 
+    // quantidade de comparações com a chave
+    analise->comppesquisa = ((double)(timepesquisa))/ CLOCKS_PER_SEC;
+
+    LiberaArvore(&Arvore); //chamada da função para liberar arvore da memória principal
+}
+
+//insere os registros nas paginas 
+void inserenaPagina(Apontadores Arvore, Item dados, Apontadores ApDir, Analise* analise){
     
     short Nhaposicao;
     int k;
     k = Arvore->quant; 
     Nhaposicao = (k > 0);
 
-    while(Nhaposicao){
+    while(Nhaposicao){// loop até que a posição seja encontrada
 
-        if(registro.chave >= Arvore->pai[k-1].chave){
-            Nhaposicao = false;
+        analise->comppre++;// acrescenta a cada comparação
+
+        // se a chave a ser inserida for maior que 
+        if(dados.chave >= Arvore->pai[k-1].chave){
+            Nhaposicao = 0;
             break;
         }
         
         Arvore->pai[k] = Arvore->pai[k-1];
         Arvore->filhos[k+1] = Arvore->filhos[k];
         k--;
-        if(K < 1)Nhaposicao = false;
+        if(k < 1)
+        Nhaposicao = 0;
     }
 
-    Arvore->pai[k] = registro;
-    Arvore->filhos[k+1] = ApDir;
-    Arvore->quant++;
+    Arvore->pai[k] = dados;
+    Arvore->filhos[k+1] = ApDir; 
+    Arvore->quant++;//os itens na pagina aumenta
 }
 
-void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apontadores* ApRetorno){
+void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apontadores* ApRetorno, Analise* analise){
 
     long i = 1;
     long j;
@@ -44,40 +93,55 @@ void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apont
         (*ApRetorno) = NULL;
         return;
     }
-    while(i < Arvore->quant && x.chave > Arvore->pai[i-1].chave) i++;
+
+// Enquanto o valor de `i` for menor que a quantidade de itens na árvore
+// e a chave do item `x` for maior que a chave do item na posição `i-1` do vetor `pai` da árvore,
+// incrementa `i` para continuar a busca na próxima posição.
+    while(i < Arvore->quant && x.chave > Arvore->pai[i-1].chave){
+        i++;
+        analise->comppre++; 
+    }
     
+    // se o item ja existir ele não é reinserido 
     if(x.chave == Arvore->pai[i-1].chave){
-        printf("Registro existente");
-        *cresceu = false;
-        return;
+            printf("Registro existente");
+            analise->comppre++;
+            *cresceu = false;
+            return;
     }
 
+    analise->comppre++;
+
+    //se a chave a ser inserida for menor que a chave da pagina
     if(x.chave < Arvore->pai[i-1].chave) i--;
-    ins(x,Arvore->filhos[i], cresceu, registroretorno, ApRetorno);
+    ins(x,Arvore->filhos[i], cresceu, registroretorno, ApRetorno, analise);
     if(!*cresceu)
         return;
+    
+    //pagina tem espaço
     if(Arvore->quant < M*M){
-        inserenaPagina(Arvore, *registroretorno, *ApRetorno);
+        inserenaPagina(Arvore, *registroretorno, *ApRetorno, analise);
         *cresceu = false;
         return;
     }
 
-    ApTemp = (Apontadores)malloc(sizeof(Pagina));
+    //dividir pagina numeros de itens maximo atingido
+    ApTemp = (Apontadores)malloc(sizeof(Pagina));//alocação de nova pagina
     ApTemp->quant = 0;
     ApTemp->filhos[0]= NULL;
 
     if(i < M+1){
-        inserenaPagina(ApTemp, Arvore->pai[M*M-1], Arvore->filhos[M*M]);
+        inserenaPagina(ApTemp, Arvore->pai[M*M-1], Arvore->filhos[M*M], analise);
         Arvore->quant--;
-        inserenaPagina(Arvore, *registroretorno, *ApRetorno);
+        inserenaPagina(Arvore, *registroretorno, *ApRetorno, analise);
     }
 
     else
-        inserenaPagina(ApTemp, *registroretorno, *ApRetorno);
+        inserenaPagina(ApTemp, *registroretorno, *ApRetorno, analise);
 
     for(j = M+2;j <= M*M; j++){
 
-        inserenaPagina(ApTemp, Arvore->pai[j-1],Arvore->filhos[j]);
+        inserenaPagina(ApTemp, Arvore->pai[j-1],Arvore->filhos[j], analise);
     
     }
 
@@ -88,14 +152,14 @@ void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apont
 }
 
 
-void insere(Item x, Apontadores* Arvore){
+void insere(Item x, Apontadores* Arvore, Analise, Analise* analise){
 
     short cresceu;
     Item registroretorno; 
     Pagina* ApRetorno, *ApTemp; 
-    ins(x, *Arvore, &cresceu, &registroretorno, &ApRetorno);
+    ins(x, *Arvore, &cresceu, &registroretorno, &ApRetorno, analise);
 
-    if(cresceu){
+    if(cresceu){//crescimento na altura da raiz
  
         ApTemp = (Pagina*) malloc(sizeof(Pagina));// alocando memória
         ApTemp->quant = 1;
@@ -118,37 +182,57 @@ void imprimirDados(Item x){
 }
 
 
-void pesquisar(Item *x, Apontadores Arvore){
+void pesquisar(Item *x, Apontadores Arvore, Analise* analise){
 
     long i = 1; 
     
-    // colocar tempo de execução, numero de quantas leituras e comparações  
-
     //Se a árvore estiver vazia 
     if(Arvore == NULL){
-        long i = 1; 
+        analise->comppesquisa++;
         printf("Nenhum registro encontrado.\n");
         return 0;
     }
 
+    analise->comppesquisa++;
     // enquanto i for menor que a quantidade de Itens na arvore e a chave for  
-    while(i < Arvore->quant && (x->chave > Arvore-> pai[i-1].chave)) i++;
+    while(i < Arvore->quant && (x->chave > Arvore-> pai[i-1].chave)){
+        i++;
+        analise->comppesquisa++;
+    } 
 
+    analise->comppesquisa++;
     // se a chave for igual ao item da árvore
     if(x->chave == Arvore->pai[i-1].chave){
         *x = Arvore->pai[i-1];
         imprimirDados(*x);
-        return 1;
+        analise->comppesquisa++;
+        return;
     }
 
+    analise->comppesquisa++;
     // se a chave for menor que item da arvore retorna chamada recurssiva pelaesquerda 
     if(x->chave < Arvore->pai[i-1].chave)
-        pesquisar(x, Arvore->filhos[i-1]);
+        pesquisar(x, Arvore->filhos[i-1], analise);
     
     // se a chave for maior chamada recurssiva para a direita
     else 
-        pesquisar(x, Arvore->filhos[i]);
+        pesquisar(x, Arvore->filhos[i], analise);
 }
 
+void LiberaPagina(Apontadores Ap){ // Funcão recurssiva para liberar cada nodo da arvore 
+    if (Ap != NULL){
+        for (int i = 0; i <= Ap->quant; i++){
+            LiberaPagina(Ap->filhos[i]); 
+        }
+        free(Ap);
+    }
+}
+
+void LiberaArvore(Apontadores *Arvore){
+    if (*Arvore != NULL){
+        LiberaPagina(*Arvore); // chamada para função recurssiva
+        *Arvore = NULL; 
+    }
+}
 
 
