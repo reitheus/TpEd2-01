c31e6bca27f3bc17dc5220a76848abe43bf6203f
Lou-LAE <136115381+Lou-LAE@users.noreply.github.com>
2024-08-02 17:30:22 -0300

    Update arvoreb.h

--- include/arvoreb.h
+++ include/arvoreb.h
@@ -2,7 +2,6 @@
 #define ARVOREB_H
 
 #include "../include/item.h"
-
 #define M 10// Ordem da árvore
 
 //criando variável ponteiro do tipo página
@@ -10,15 +9,18 @@ typedef struct Pagina* Apontadores;
 
 typedef struct Pagina {
     short quant;// quantidades de itens nos vetores 
-    Item pai[M*M];// vetor de itens 
+    Item pai[M*M];// vetor de itens na página
     Apontadores filhos[(M*M) + 1]; // vetor de apontadores 
 } Pagina; 
 
-void inicializa(Apontadores Arvore);
-void pesquisar(Item* x, Apontadores Arvore);
-void insere(Item x, Apontadores* Arvore); 
-void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apontadores* ApRetorno);
-void inserenaPagina(Apontadores Arvore, Item dados, Apontadores ApDir);
-void imprimir(Item x);
+void inicializa(Apontadores* Arvore);
 
-#endif// arvorebEst.h
+//void arvoreB(Analise*, FILE*, TipoRegistro *, int , int, int);
+
+void pesquisar(Item* x, Apontadores Arvore, Analise*);
+void insere(Item x, Apontadores* Arvore, Analise*); 
+void ins(Item x,Apontadores Arvore, short *cresceu, Item* registroretorno, Apontadores* ApRetorno, Analise*);
+void inserenaPagina(Apontadores Arvore, Item dados, Apontadores ApDir, Analise*);
+void imprimir(Item x); // impressão das informações da chave encontrada. 
+
+#endif
