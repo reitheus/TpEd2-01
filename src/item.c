36b677704612abb52a14aad3cea4ce97ba5394d5
reitheus <matheus.mota.gomes@hotmail.com.br>
2024-08-08 01:43:21 -0300

    Release-08/08/2024_01:43

--- src/item.c
+++ src/item.c
@@ -1,32 +1,142 @@
 #include "../include/item.h"
+#include "../include/graficos.h"
+void impPre(DadosPesquisa *dados);
+void impPesq(DadosPesquisa *dados);
 
+void impPre(DadosPesquisa *dados){
+  //titulo 1
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i <= 41;i++)printf(" ");
+  printf(BOLD("Pré - Processamento:"));
+  for(int i = 0;i < 45;i++)printf(" ");
+  printf(TAB_VER"\n");
+
+  //quantidade
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Quantidade de comparações no pré processamento = %-10d",dados->analise.comppre);
+  for(int i = 0;i < 27;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //transferencias
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Quantidade de transferencias no Pré-processamento = %-10d", dados->analise.transpre);
+  for(int i = 0;i < 24;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //tempo
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Tempo de execução do pré processamento = %-lf segundos", dados->analise.timepre);
+  for(int i = 0;i < 28;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //linha debaixo
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_ML);
+  for (int i = 0; i < 107; i++)printf(TAB_HOR);
+  printf(TAB_MR"\n");
+
+}
+
+void impPesq(DadosPesquisa *dados){
+
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i <= 46;i++)printf(" ");
+  printf(BOLD("Pesquisa: "));
+  for(int i = 0;i < 50;i++)printf(" ");
+  printf(TAB_VER"\n");
+
+  //quantidade
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Quantidade de comparações na pesquisa = %-10d", dados->analise.comppesquisa);
+  for(int i = 0;i < 36;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //transferencias
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Quantidade de transferencias na pesquisa = %-10d", dados->analise.transpesquisa);
+  for(int i = 0;i < 33;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //tempo
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_VER);
+  for(int i = 0;i < 21;i++)printf(" ");
+  printf("Tempo de execução na pesquisa = %-lf segundos", dados->analise.timepesquisa);
+  for(int i = 0;i < 37;i++)printf(" ");
+  printf(TAB_VER"\n");
+  //linha debaixo
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_BL);
+  for (int i = 0; i < 107; i++)printf(TAB_HOR);
+  printf(TAB_BR"\n");
+}
 void imprimeDado(DadosPesquisa *dado, Item *entrada) {
-    printf("|  Pré - Processamento: \n");
-    printf("|  Quantidade de comparações no pré processamento = %d\n", dado->analise.comppre);
-    printf("|  Quantidade de transferencias no Pré processamento = %d\n", dado->analise.transpre);
-    printf("|  Tempo de execução do pré processamento = %lf segundos\n", dado->analise.timepre);
-    printf("-----------------------------------------------------------------------------------------\n");
-    printf("|  Pesquisa: \n");
-    printf("|  Quantidade de comparações na pesquisa = %d\n", dado->analise.comppesquisa);
-    printf("|  Quantidade de transferencias na pesquisa = %d\n", dado->analise.transpesquisa);
-    printf("|  Tempo de execução na pesquisa = %lf segundos\n", dado->analise.timepesquisa);
-    printf("-----------------------------------------------------------------------------------------\n");
+
+  impPre(dado);
+  impPesq(dado);
+  
 }
 
 void impreResultado(bool pesq,DadosPesquisa *dado,Item* entrada){
+  
+  printf("\n");
+  for(int i = 0;i<=54;i++)printf(" ");
+  printf(TAB_TL);
+  for (int i = 0; i < 107; i++){
+    printf(TAB_HOR);
+  }
+  printf(TAB_TR"\n");
 
   if (pesq) {
-    printf("\n");
-    printf("-----------------------------------------------------------------------------------------\n");
-    printf("|  Encontrou a Chave!  \n");
-    printf("-----------------------------------------------------------------------------------------\n");
+    //escrita
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 107; i++)printf(" ");
+    printf(TAB_VER"\n");
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 42; i++)printf(" ");
+    printf(GREEN(BOLD(" Encontrou a Chave!")));
+    for (int i = 0; i < 46; i++)printf(" ");
+    printf(TAB_VER"\n");
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 107; i++)printf(" ");
+    printf(TAB_VER"\n");
+    //linha debaixo
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_ML);
+    for (int i = 0; i < 107; i++)printf(TAB_HOR);
+    printf(TAB_MR"\n");
     imprimeDado(dado, entrada);
     printf("\n");
   } else {
-    printf("\n");
-    printf("-----------------------------------------------------------------------------------------\n");
-    printf("|  Não encontrou a Chave!  \n");
-    printf("-----------------------------------------------------------------------------------------\n");
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 107; i++)printf(" ");
+    printf(TAB_VER"\n");
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 40; i++)printf(" ");
+    printf(RED(BOLD("Não encontrou a Chave!")));
+    for (int i = 0; i <= 44; i++)printf(" ");
+    printf(TAB_VER"\n");
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_VER);
+    for (int i = 0; i < 107; i++)printf(" ");
+    printf(TAB_VER"\n");
+    //linha debaixo
+    for(int i = 0;i<=54;i++)printf(" ");
+    printf(TAB_ML);
+    for (int i = 0; i < 107; i++)printf(TAB_HOR);
+    printf(TAB_MR"\n");
     imprimeDado(dado, entrada);
     printf("\n");
   }
